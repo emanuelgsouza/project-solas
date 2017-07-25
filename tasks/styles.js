73a0fb3b0beb2e7b28c00e4ce8d44ac6ef77c306
Emanuel Gonçalves <emanuelgdesouza@gmail.com>
2017-07-24 22:48:58 -0300

    Inserindo autoprefixer no projeto

--- tasks/styles.js
+++ tasks/styles.js
@@ -3,11 +3,18 @@ const minifyCss = require('gulp-clean-css')
 const stylus = require('gulp-stylus')
 const gulpif = require('gulp-if')
 const rename = require('gulp-rename')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
 
 module.exports = function (args, reload) {
   gulp.task('stylus', function () {
     gulp.src('./src/assets/stylus/app.styl')
       .pipe(stylus())
+      .pipe(postcss([
+        autoprefixer({
+          browsers: ['last 9 versions']
+        })
+      ]))
       .pipe(gulpif(args.production, gulp.dest('./dist/css/')))
       .pipe(gulpif(args.production, minifyCss()))
       .pipe(gulpif(args.production, rename('app.min.css')))
